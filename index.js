411b9399c4c487574f4901968c550113519992cb
wilperezagu <wmperez852@gmail.com>
2022-06-30 08:22:17 -0500

    practica heroku3

--- index.js
+++ index.js
@@ -9,17 +9,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
-const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
-    }
-  }
-}
-app.use(cors(options));
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
